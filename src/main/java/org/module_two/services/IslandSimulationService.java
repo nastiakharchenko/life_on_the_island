b5651f06806d1a2d3b899731c986cf99f8fe2eba
nastiakharchenko <nastia200115@gmail.com>
2024-09-19 16:27:40 +0300

    Second commit

--- src/main/java/org/module_two/services/IslandSimulationService.java
+++ src/main/java/org/module_two/services/IslandSimulationService.java
@@ -1,12 +1,11 @@
 package org.module_two.services;
 
 import lombok.Getter;
+import lombok.extern.log4j.Log4j;
 import org.module_two.entity.Island;
-import org.module_two.services.task.AnimalHpControlTask;
-import org.module_two.services.task.AnimalLifecycleTask;
-import org.module_two.services.task.IslandStatisticsTask;
-import org.module_two.services.task.PlantGrowthTask;
+import org.module_two.services.task.*;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,6 +14,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import static org.module_two.constants.SystemIslandConstants.*;
 
+@Log4j
 public class IslandSimulationService {
     private static volatile IslandSimulationService islandSimulation;
     @Getter
@@ -38,18 +38,30 @@ public class IslandSimulationService {
     }
 
     public void run() {
-        executorService = Executors.newScheduledThreadPool(4);
+        executorService = Executors.newScheduledThreadPool(6);
 
-        AnimalLifecycleTask animalLifecycleTask = new AnimalLifecycleTask();
+        CountDownLatch latch = new CountDownLatch(3);   //средство синхронизации, позволяющее одному
+                                                        // или нескольким потокам ожидать завершения набора операций,
+                                                        // выполняемых в других потоках
+        AnimalEatTask animalEatTask = new AnimalEatTask(latch);
+        AnimalReproduceTask animalReproduceTask = new AnimalReproduceTask(latch);
+        AnimalMovementTask animalMovementTask = new AnimalMovementTask(latch);
+        AnimalHpControlTask animalHpControlTask = new AnimalHpControlTask();
         PlantGrowthTask plantGrowthTask = new PlantGrowthTask();
         IslandStatisticsTask islandStatisticsTask = new IslandStatisticsTask();
-        AnimalHpControlTask animalHpControlTask = new AnimalHpControlTask();
 
-        executorService.scheduleAtFixedRate(animalLifecycleTask, 0
-                , durationSimulation.get() / 3 == 0 ? 1 : durationSimulation.get() / 3
-                , TimeUnit.SECONDS);   //3 раза в сутки
+        long durationSimulationRegular = durationSimulation.get() / 3 == 0 ? 1 : durationSimulation.get() / 3;          //3 раза в условные сутки
+        executorService.scheduleAtFixedRate(animalEatTask, 0, durationSimulationRegular, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(animalReproduceTask, 0, durationSimulationRegular, TimeUnit.SECONDS);
+        executorService.scheduleAtFixedRate(animalMovementTask, 0, durationSimulationRegular, TimeUnit.SECONDS);
+        try {
+            latch.await();      //ожидание обнуления счетчика
+        } catch (InterruptedException e) {
+            log.error(this.getClass() + " " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+        executorService.scheduleAtFixedRate(animalHpControlTask, 0, durationSimulation.get(), TimeUnit.SECONDS);  //раз в сутки
         executorService.scheduleAtFixedRate(plantGrowthTask, 0, durationSimulation.get(), TimeUnit.SECONDS);      //раз в сутки
         executorService.scheduleAtFixedRate(islandStatisticsTask, 0, durationSimulation.get(), TimeUnit.SECONDS); //раз в сутки
-        executorService.scheduleAtFixedRate(animalHpControlTask, 0, durationSimulation.get(), TimeUnit.SECONDS);  //раз в сутки
     }
 }
